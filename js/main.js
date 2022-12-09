914d2d8374660171814bbaae7c9d93d7fc984c31
ZoiloMendoza <zoilomm@hotmail.com>
2022-12-09 01:13:37 -0600

    dando ancho fijo

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const collar = document.getElementById('collar');
 const keyRow = document.getElementsByClassName('keyboard');
 let collarColores = ['color1','color2','color3','color4','color5','color6'];
 
-window.elegirColor = (color,id) => {
+elegirColor = (color,id) => {
     collarColores.map((collarColor) => {
         if(collarColor === id) {
             collar.style.background = color;
@@ -10,7 +10,7 @@ window.elegirColor = (color,id) => {
     })
 }
 
-window.elegirColork = (color,id) => {
+elegirColork = (color,id) => {
     let keys = Array.from(keyRow);
     keys.forEach((key) => {
             key.style.color = color;
