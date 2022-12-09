041b92702946fb7ec1783302817f8648d1c7600f
ZoiloMendoza <zoilomm@hotmail.com>
2022-12-09 16:43:21 -0600

    agregando funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const collar = document.getElementById('collar');
 const keyRow = document.getElementsByClassName('keyboard');
-
+const keyboardContainer = document.querySelector('.keyboard-container');
+const borrar = document.querySelector('.delete');
+let prueba = [];
+let color = '#702F6E';
 
 elegirColor = (e) => {
     e.preventDefault();
@@ -11,6 +14,24 @@ elegirColork = (e) => {
     e.preventDefault();
     let keys = Array.from(keyRow);
     keys.forEach((key) => {
-            key.style.color = e.target.name;
-    }) 
+        key.style.color = e.target.name;  
+    })
+    return color = e.target.name;
+}
+keyboardContainer.addEventListener('click', agregarItem);
+//borrar.addEventListener('click', borrandoItem);
+
+function agregarItem (e) {
+    e.preventDefault();
+
+    let claseLetra = e.target.classList[0];
+    let letra = e.target.innerText;
+    
+    if(e.target.classList[0] == 'keyboard' && prueba.length < 9){
+        const elemento = `<div style="font-family:'Quicksand'; font-size: 65px;
+        font-weight: bolder; color: ${color}; width: 11%; height: 45px; display: flex; justify-content: center;
+        align-items: center; padding-bottom: 2px">${letra}</div>`;
+        collar.insertAdjacentHTML('beforeend', elemento);
+        prueba.push(elemento);
+    }
 }
