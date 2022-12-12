a000140c45bb39f09c51f2af3e98482a36589455
ZoiloMendoza <zoilomm@hotmail.com>
2022-12-12 02:44:02 -0600

    agregando hover a teclado, agregando funcion de eliminar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const collar = document.getElementById('collar');
 const keyRow = document.getElementsByClassName('keyboard');
 const keyboardContainer = document.querySelector('.keyboard-container');
-const borrar = document.querySelector('.delete');
+const borrar = document.getElementById('delete');
 let prueba = [];
 let color = '#702F6E';
 
@@ -19,19 +19,23 @@ elegirColork = (e) => {
     return color = e.target.name;
 }
 keyboardContainer.addEventListener('click', agregarItem);
-//borrar.addEventListener('click', borrandoItem);
+borrar.addEventListener('click', borrandoItem);
 
 function agregarItem (e) {
     e.preventDefault();
-
     let claseLetra = e.target.classList[0];
     let letra = e.target.innerText;
-    
-    if(e.target.classList[0] == 'keyboard' && prueba.length < 9){
+    if(claseLetra == 'keyboard' && prueba.length < 9){
         const elemento = `<div style="font-family:'Quicksand'; font-size: 65px;
         font-weight: bolder; color: ${color}; width: 11%; height: 45px; display: flex; justify-content: center;
         align-items: center; padding-bottom: 2px">${letra}</div>`;
         collar.insertAdjacentHTML('beforeend', elemento);
         prueba.push(elemento);
+
     }
+}
+
+function borrandoItem (e) {
+    e.preventDefault();
+    collar.removeChild(collar.lastChild)
 }
