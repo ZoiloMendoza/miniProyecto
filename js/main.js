3e0f5e6a596a5e578aa7eb8073892066c8aca0a0
ZoiloMendoza <zoilomm@hotmail.com>
2022-12-13 06:16:04 -0600

    funcionalidad elegir tamaño de collar agregada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+const sizeCollar = document.getElementsByClassName('sizeC');
 const collar = document.getElementById('collar');
 const keyRow = document.getElementsByClassName('keyboard');
 const keyboardContainer = document.querySelector('.keyboard-container');
 const borrar = document.getElementById('delete');
-let prueba = [];
+let historico = [];
 let color = '#702F6E';
+
+elegirTam = (e) => {
+    e.preventDefault();
+    let medidas = Array.from(sizeCollar);
+    medidas.forEach((medida) =>{
+        medida.style.background = '#ffffff';
+        if(medida.innerText === e.target.innerText){
+            e.target.style.background = '#c3b8b8e8';
+        }
+    })
+}
 
 elegirColor = (e) => {
     e.preventDefault();
@@ -18,6 +30,8 @@ elegirColork = (e) => {
     })
     return color = e.target.name;
 }
+
+
 keyboardContainer.addEventListener('click', agregarItem);
 borrar.addEventListener('click', borrandoItem);
 
@@ -25,19 +39,21 @@ function agregarItem (e) {
     e.preventDefault();
     let claseLetra = e.target.classList[0];
     let letra = e.target.innerText;
-    if(claseLetra == 'keyboard' && prueba.length < 9){
+    if(claseLetra == 'keyboard' && historico.length < 9){
         const elemento = `<div style="font-family:'Quicksand'; font-size: 65px;
         font-weight: bolder; color: ${color}; width: 11%; height: 45px; display: flex; justify-content: center;
         align-items: center; padding-bottom: 2px">${letra}</div>`;
         collar.insertAdjacentHTML('beforeend', elemento);
-        prueba.push(elemento);
-
+        historico.push(elemento);
     }
 }
 
 function borrandoItem (e) {
     e.preventDefault();
     if(collar.children.length > 0){
-        collar.removeChild(collar.lastChild)
+        collar.removeChild(collar.lastChild);
+    }
+    if(historico.length > 0){
+        historico.pop();
     }
 }
